fef77d5674f5c62b41b7822247bb935984e6971c
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-01-29 11:23:02 +0100

    exo fini

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function verif(){
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
+    if (password == confirmPassword){
+      document.getElementById('password').style.border = '3px solid green';
+      document.getElementById('confirmPassword').style.border = '3px solid green';
+
+}else{
+  document.getElementById('password').style.border = '3px solid red';
+  document.getElementById('confirmPassword').style.border = '3px solid red';
+}
+}
