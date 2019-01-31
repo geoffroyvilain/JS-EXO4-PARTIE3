48447e497ac09f9186ae9d1b1642cadc6f1c2e0c
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-01-31 11:37:11 +0100

    exo corrigé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 function verif(){
-    var password = document.getElementById('password').value;
-    var confirmPassword = document.getElementById('confirmPassword').value;
-    if (password == confirmPassword){
-      document.getElementById('password').style.border = '3px solid green';
-      document.getElementById('confirmPassword').style.border = '3px solid green';
-
-}else{
-  document.getElementById('password').style.border = '3px solid red';
-  document.getElementById('confirmPassword').style.border = '3px solid red';
-}
+//on déclarare la variable password et on stocke le résultat en cherchant la valeur de password
+  var password = document.getElementById('password').value;
+// idem pour confirmPassword
+  var confirmPassword = document.getElementById('confirmPassword').value;
+// si password est egal à confirmPassword alors appliquer sur value les bordures
+  if (password == confirmPassword){
+    document.getElementById('password').style.border = '3px solid green';
+    document.getElementById('confirmPassword').style.border = '3px solid green';
+// sinon appliquer sur value pour les bordures
+  }else{
+    document.getElementById('password').style.border = '3px solid red';
+    document.getElementById('confirmPassword').style.border = '3px solid red';
+  }
 }
